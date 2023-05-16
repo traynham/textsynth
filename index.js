e376c09d6fa54ab45f39d6a935c062dcc280e667
traynham <jesse@traynham.com>
2023-05-16 01:04:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import your main module
+import TextSynth, { createTextSynth, expressTextSynthEngine } from './src/merger.js';
+
+// Export your main module and any additional exports
+export default TextSynth;
+export { createTextSynth, expressTextSynthEngine };
