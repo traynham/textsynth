353e3a141d91bc2f3a60c23a315eec64e0439716
traynham <jesse@traynham.com>
2023-07-08 13:32:28 -0500

    Initial commit.

---
+++ tests/__tests__/text/repeat.js
@@ -0,0 +1,30 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	testString: 'abc',
+	emptyString: '',
+	nonString: 12345
+}
+
+describe('repeat plugin', () => {
+
+	test('repeats a string a specified number of times', () => {
+		const input = "[repeat(3) testString]"
+		expect(textSynth.merge(input, payload)).toBe('abcabcabc')
+	})
+
+	test('returns an empty string when repeating an empty string', () => {
+		const input = "[repeat(3) emptyString]"
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
+
+	// test('throws an error when repeating a non-string', () => {
+	// 	const input = "[repeat(3) nonString]"
+	// 	expect(() => textSynth.merge(input, payload)).toThrow(TypeError)
+	// })
+	
+})
