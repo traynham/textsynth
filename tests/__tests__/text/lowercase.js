8ff4d9ebdb87d2eab6f94c9c1628fa261e861fac
traynham <jesse@traynham.com>
2023-07-08 13:32:23 -0500

    Initial commit.

---
+++ tests/__tests__/text/lowercase.js
@@ -0,0 +1,29 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	testString: 'THIS IS A TEST',
+	emptyString: '',
+	nonString: 12345
+}
+
+describe('lowercase plugin', () => {
+
+	test('converts a string to lowercase', () => {
+		const input = "[lowercase testString]"
+		expect(textSynth.merge(input, payload)).toBe('this is a test')
+	})
+
+	test('returns an empty string when provided with an empty string', () => {
+		const input = "[lowercase emptyString]"
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
+
+	test('returns the original content if it is not a string', () => {
+		const input = "[lowercase nonString]"
+		expect(textSynth.merge(input, payload)).toBe('12345')
+	})
+})
