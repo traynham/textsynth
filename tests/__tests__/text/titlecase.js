bfd6ab63227b7f085486337f4db55d9bff4c7d4e
traynham <jesse@traynham.com>
2023-07-08 13:32:46 -0500

    Initial commit.

---
+++ tests/__tests__/text/titlecase.js
@@ -0,0 +1,24 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	testString: 'this is a test string',
+	nonString: 12345
+}
+
+describe('titlecase plugin', () => {
+
+	test('returns a string with each word capitalized', () => {
+		const input = "[titlecase testString]"
+		expect(textSynth.merge(input, payload)).toBe('This Is A Test String')
+	})
+
+	test('throws an error when trying to apply titlecase to a non-string', () => {
+		const input = "[titlecase nonString]"
+		expect(textSynth.merge(input, payload)).toBe(`${payload.nonString}`)
+	})
+	
+})
