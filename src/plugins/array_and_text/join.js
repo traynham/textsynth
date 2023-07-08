31f8962214dd6491e9d73bbe589cb35e316d2784
traynham <jesse@traynham.com>
2023-07-08 13:19:02 -0500

    General updates.

--- src/plugins/array_and_text/join.js
+++ src/plugins/array_and_text/join.js
@@ -1,11 +1,49 @@
 export default {
 	name: 'join',
 	description: 'Joins the elements of an array with a specified separator.',
-	example: '{{join(", "): property.path}}',
-	usage: '{{join(", "): site.items}}',
+	_example: '[join(", "): property.path]',
+	usage: '[join(delimiter) site.items]',
+	
+	examples: [
+		{
+			code: "[join('-') site.items]",
+			result: '1-2-3 //Given that site.items is an array like [1,2,3]'
+		},
+		{
+			code: "[join(', ') fruit]",
+			result: `Apple, Orange, Grape //Given that site.items is an array like ['Apple', 'Organge', 'Grape']`
+		},
+		{
+			code: "[join(', ') fruit]",
+			result: `AppleOrangeGrape //Given that site.items is an array like ['Apple', 'Organge', 'Grape']`
+		}
+	],
+	
+	settings: {
+		delimiter: ', ' // Default delimiter to use to join.
+	},
+	
+	params: [
+		{
+			name: 'delimiter',
+			type: 'string',
+			required: false,
+			description: 'String to join items with.'
+		}
+	],
+	
 	category: 'Array/Text',
+	kind: 'single',
 	type: ['array'],
 	processor(req){
-		return req.content.join(req.params[0])
+		
+		if (typeof req.content === 'string') {
+			return req.content
+		}
+		
+		let delimiter = req.params[0] || this.settings.delimiter
+		
+		return req.content?.join(delimiter)
+		
 	}
 }
