5bfa8f47fef0eb582d88ef46204ace49b3019527
traynham <jesse@traynham.com>
2023-07-08 13:22:17 -0500

    General updates.

--- src/plugins/text/uppercase.js
+++ src/plugins/text/uppercase.js
@@ -4,8 +4,17 @@ export default {
 	example: '{{uppercase: property.path}}',
 	usage: '{{uppercase: site.title}}',
 	category: 'Text',
+	content: [
+		{
+			name: 'content',
+			type: 'any',
+			required: true,
+			description: 'The text to be transformed.'
+		}
+	],
 	type: 'string',
 	kind: 'single',
+	aliases: ['upper', 'up'],
 	processor(req) {
 		return req.content.toUpperCase()
 	}
