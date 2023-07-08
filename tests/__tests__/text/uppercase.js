1820c956fc08084bcd22052f5e9524c88742ea21
traynham <jesse@traynham.com>
2023-07-08 13:33:04 -0500

    Initial commit.

---
+++ tests/__tests__/text/uppercase.js
@@ -0,0 +1,28 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	lowercaseString: 'this is a test',
+	mixedcaseString: 'This IS a Test'
+}
+
+describe('uppercase plugin', () => {
+
+	test('converts a lowercase string to uppercase', () => {
+		const input = "[uppercase lowercaseString]"
+		expect(textSynth.merge(input, payload)).toBe('THIS IS A TEST')
+	})
+
+	test('converts a mixed case string to uppercase', () => {
+		const input = "[uppercase mixedcaseString]"
+		expect(textSynth.merge(input, payload)).toBe('THIS IS A TEST')
+	})
+
+	test('returns the same string when already in uppercase', () => {
+		const input = "[uppercase 'HELLO']"
+		expect(textSynth.merge(input, payload)).toBe('HELLO')
+	})
+})
