95fe8c6d37607210e61bcb5ff81b0764a5a38aa9
traynham <jesse@traynham.com>
2023-07-08 13:28:13 -0500

    Disabled for now.

--- src/plugins/url/decodeURIComponent.js
+++
@@ -1,9 +0,0 @@
-export default {
-	name: 'decodeURIComponent',
-	description: 'Applies the JavaScript decodeURIComponent() function to a string value.',
-	example: '{{decodeURIComponent: property.path}}',
-	category: 'URL',
-	processor({content}) {
-		return decodeURIComponent(content);
-	}
-}
--- src/plugins/url/encodeURIComponent.js
+++
@@ -1,9 +0,0 @@
-export default {
-	name: 'encodeURIComponent',
-	description: 'Applies the JavaScript encodeURIComponent() function to a string value.',
-	example: '{{encodeURIComponent: property.path}}',
-	category: 'URL',
-	processor({content}) {
-		return encodeURIComponent(content);
-	}
-}
