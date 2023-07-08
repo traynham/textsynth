fa37f082d40de2098aacc738b7ea61bd58094fe2
traynham <jesse@traynham.com>
2023-07-08 13:19:06 -0500

    General updates.

--- src/plugins/array_and_text/length.js
+++ src/plugins/array_and_text/length.js
@@ -1,8 +1,18 @@
 const plugin = {
 	name: 'length',
 	description: 'Returns the length of a string or the number of elements in an array.',
-	example: '{{length: property.path}}',
-	usage: '{{length: site.name}}',
+	_example: '{{length: property.path}}',
+	examples: [
+		{
+			code: "[length 'This is 10']",
+			result: '10'
+		},
+		{
+			code: "[length found_array]",
+			result: '15 (Assuming there are 15 array items)'
+		}
+	],
+	usage: '[length items]',
 	category: 'Array/Text',
 	type: ['string', 'array'],
 	processor(req) {
