860a605b4dbba48265c0f73f2c9f678406653d82
traynham <jesse@traynham.com>
2023-07-08 13:32:05 -0500

    Initial commit.

---
+++ tests/__tests__/merger/_gather_cargo.js
@@ -0,0 +1,78 @@
+import { describe, expect, test } from '@jest/globals'
+
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+const payload = { testKey: 'testValue' }
+
+describe('_gather_cargo method', () => {
+	
+	test('correctly gathers cargo', () => {
+		
+		const args = `#myId .myClass tess='ting' "plainValue" testKey`
+		
+		const result = textSynth._gather_cargo(args, payload)
+		
+		expect(result).toEqual({
+			attributes: { tess: 'ting' },
+			classes: [ 'myClass' ],
+			id: 'myId',
+			values: [ 'plainValue', 'testKey' ],
+			params: [ 'plainValue', 'testValue' ]
+		})
+		
+	})
+
+	test('returns undefined when no args are provided', () => {
+		const result = textSynth._gather_cargo(null, payload)
+		expect(result).toBeUndefined()
+	});
+
+	test('handles args with only attributes', () => {
+		const args = 'id="myId"'
+		
+		const result = textSynth._gather_cargo(args, payload)
+		
+		expect(result).toEqual({
+			attributes: { id: 'myId' },
+			classes: [],
+			id: '',
+			values: [],
+			params: []
+		})
+		
+	})
+
+	test('handles args with only classes', () => {
+		
+		const args = '.myClass'
+		
+		const result = textSynth._gather_cargo(args, payload)
+		
+		expect(result).toEqual({
+			attributes: {},
+			classes: ['myClass'],
+			id: '',
+			values: [],
+			params: []
+		})
+		
+	})
+
+	test('handles args with only values', () => {
+		
+		const args = 'plainValue'
+		
+		const result = textSynth._gather_cargo(args, payload)
+		
+		expect(result).toEqual({
+			attributes: {},
+			classes: [],
+			id: '',
+			values: ['plainValue'],
+			params: ['plainValue']
+		})
+		
+	})
+
+})
