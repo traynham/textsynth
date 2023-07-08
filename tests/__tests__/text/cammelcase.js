ce060542486ab24c25604888dfd819c793dfc147
traynham <jesse@traynham.com>
2023-07-08 13:32:16 -0500

    Initial commit.

---
+++ tests/__tests__/text/cammelcase.js
@@ -0,0 +1,23 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	testString: 'This is a test',
+	nonString: 12345
+}
+
+describe('camelCase plugin', () => {
+
+	test('converts a string to camelCase', () => {
+		const input = "[camelcase testString]"
+		expect(textSynth.merge(input, payload)).toBe('thisIsATest')
+	})
+
+	test('returns the original content if it is not a string', () => {
+		const input = "[camelcase nonString]"
+		expect(textSynth.merge(input, payload)).toBe('12345')
+	})
+})
