3936acaca2133fe8fab6f2cc562a5a49c1aaeea9
traynham <jesse@traynham.com>
2023-07-08 13:32:08 -0500

    Initial commit.

---
+++ tests/__tests__/merger/_getApproot.js
@@ -0,0 +1,50 @@
+import fs from 'fs'
+//import path from 'path'
+import process from 'process'
+import { describe, expect, jest, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+describe('_getAppRoot method', () => {
+  
+  test('returns correct directory if node_modules is present', async () => {
+	const textSynth = await TextSynth()
+
+	const originalCwd = process.cwd
+	const originalReaddirSync = fs.readdirSync
+	
+	// Mock process.cwd to return a custom current directory
+	process.cwd = jest.fn().mockReturnValue('/path/to/current')
+	
+	// Mock fs.readdirSync to simulate a directory with node_modules
+	fs.readdirSync = jest.fn().mockReturnValue(['node_modules'])
+	
+	const result = textSynth._getAppRoot()
+
+	expect(result).toBe('/path/to/current')
+
+	// Reset the mocks
+	process.cwd = originalCwd
+	fs.readdirSync = originalReaddirSync
+  })
+
+  test('returns null if no parent directory with node_modules is found', async () => {
+	const textSynth = await TextSynth()
+
+	const originalCwd = process.cwd
+	const originalReaddirSync = fs.readdirSync
+	
+	// Mock process.cwd to return a custom current directory
+	process.cwd = jest.fn().mockReturnValue('/path/to/current')
+	
+	// Mock fs.readdirSync to simulate a directory without node_modules
+	fs.readdirSync = jest.fn().mockReturnValue(['other_dir'])
+	
+	const result = textSynth._getAppRoot()
+
+	expect(result).toBe(null)
+
+	// Reset the mocks
+	process.cwd = originalCwd
+	fs.readdirSync = originalReaddirSync
+  })
+})
