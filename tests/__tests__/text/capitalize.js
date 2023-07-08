e694cd8999fba0c8f3d27562cde65f90edadf0a0
traynham <jesse@traynham.com>
2023-07-08 13:32:19 -0500

    Initial commit.

---
+++ tests/__tests__/text/capitalize.js
@@ -0,0 +1,30 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	testString: 'this is a test',
+	emptyString: '',
+	nonString: 12345
+}
+
+describe('capitalize plugin', () => {
+
+	test('capitalizes the first letter of a string', () => {
+		const input = "[capitalize testString]"
+		expect(textSynth.merge(input, payload)).toBe('This is a test')
+	})
+
+	test('returns an empty string when provided with an empty string', () => {
+		const input = "[capitalize emptyString]"
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
+
+	// test('returns the original content if it is not a string', () => {
+	// 	const input = "[capitalize nonString]"
+	// 	expect(textSynth.merge(input, payload)).toBe('12345')
+	// })
+	
+})
