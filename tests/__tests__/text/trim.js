873d1bbe52a2ab1418ce5b3f58c8486a546f50f9
traynham <jesse@traynham.com>
2023-07-08 13:32:50 -0500

    Initial commit.

---
+++ tests/__tests__/text/trim.js
@@ -0,0 +1,24 @@
+import { describe, expect, test } from '@jest/globals'
+import TextSynth from '../index.js'
+
+const textSynth = await TextSynth()
+
+// Test payload
+const payload = {
+	testString: '  this is a test string  ',
+	nonString: 12345
+}
+
+describe('trim plugin', () => {
+
+	test('returns a string with leading and trailing whitespace removed', () => {
+		const input = "[trim testString]"
+		expect(textSynth.merge(input, payload)).toBe('this is a test string')
+	})
+
+	// test('throws an error when trying to trim a non-string', () => {
+	// 	const input = "[trim nonString]"
+	// 	expect(() => textSynth.merge(input, payload)).toThrow(TypeError)
+	// })
+	
+})
