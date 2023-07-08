3cc06360efba300c3c34962359a1051dc75a5a79
traynham <jesse@traynham.com>
2023-07-08 13:33:11 -0500

    Initial commit.

---
+++ tests/__tests__/util/log.js
@@ -0,0 +1,31 @@
+import { expect, describe, test, jest } from '@jest/globals';
+import TextSynth from '../index.js';
+
+const textSynth = await TextSynth()
+textSynth.setDelimiters({ opener: '{{', closer: '}}' })
+
+describe('log plugin', () => {
+
+	test('logs the correct content to console', () => {
+		const logMock = jest.spyOn(console, 'log');
+		
+		const input = '{{log: "Hello, World!"}}';
+		textSynth.merge(input);
+
+		expect(logMock).toHaveBeenCalledWith('Hello, World!');
+
+		logMock.mockRestore();
+	});
+
+	test('logs object correctly', () => {
+		const logMock = jest.spyOn(console, 'log');
+
+		const payload = { property: { path: { key: "value" } } };
+		const input = '{{log: property.path}}';
+		textSynth.merge(input, payload);
+
+		expect(logMock).toHaveBeenCalledWith({ key: "value" });
+
+		logMock.mockRestore();
+	});
+});
