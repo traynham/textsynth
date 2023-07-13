359b7885cad2b88e65bcdcdd1707cc6f1fef7902
traynham <jesse@traynham.com>
2023-07-13 16:34:29 -0500

    Updated metadata.

--- src/plugins/text/trim.js
+++ src/plugins/text/trim.js
@@ -1,10 +1,36 @@
 export default {
+	
+	// Basic Information
 	name: 'trim',
-	description: 'Trims whitespace from the beginning and end of a string.',
-	example: '{{trim: property.path}}',
-	usage: '{{trim: site.title}}',
+	author: 'Jesse Traynham',
 	category: 'Text',
+	description: 'Trims whitespace from the beginning and end of a string.',
+	kind: 'single',
+	syntax: '[trim: property.path]',
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'property.path',
+			type: 'string',
+			required: true,
+			description: 'The path of the property to trim.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			payload: { example: ' example trim ' },
+			input: "[trim: example]",
+			output: "example trim",
+			note: 'This example trims the whitespace from the string " example trim ".'
+		}
+	],
+
 	processor({content}) {
 		return content.trim()
 	}
+
 }
