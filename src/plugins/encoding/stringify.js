de09eef882cf36b46cc06b9795d558e95a1e4607
traynham <jesse@traynham.com>
2023-07-13 16:30:25 -0500

    Updated metadata.

--- src/plugins/encoding/stringify.js
+++ src/plugins/encoding/stringify.js
@@ -1,10 +1,37 @@
 export default {
 	name: 'stringify',
-	description: 'Converts a JavaScript value to a JSON string.',
-	example: '{{stringify: property}}',
-	usage: '{{stringify: site}}',
+	author: 'Jesse Traynham',
+	version: '1.0.0',
+	description: 'Converts a JavaScript value (such as an object, array, number, or string) to a JSON string.',
+	examples: [
+		{
+			payload: `{ stringy: { key: 'value', key2: 'value2' } }`,
+			input: "[stringify stringy]",
+			output: '{"key":"value","key2":"value2"}',
+			note: 'Converts an object to a JSON string.'
+		},
+		{
+			payload: `{ fruit: ['apple', 'orange', 'banana'] }`,
+			input: "[stringify fruit]",
+			output: '["apple","orange","banana"]',
+			note: 'Converts an array to a JSON string.'
+		},
+		{
+			input: "[stringify 'Hello, World!']",
+			output: '"Hello, World!"',
+			note: 'Converts a string to a JSON string.'
+		},
+	],
+	syntax: '[stringify value]',
+	content: [
+		{
+			name: 'content',
+			type: 'any',
+			required: true,
+			description: 'The JavaScript value to be converted to a JSON string.'
+		}
+	],
 	category: 'Encoding',
-	type: 'string',
 	kind: 'single',
 	processor(req) {
 		return JSON.stringify(req.content)
