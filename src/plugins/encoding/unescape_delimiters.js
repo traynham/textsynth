910d0e48e3fa99871c179ea39b7fc70f1f92324d
traynham <jesse@traynham.com>
2023-07-13 16:30:35 -0500

    Updated metadata.

--- src/plugins/encoding/unescape_delimiters.js
+++ src/plugins/encoding/unescape_delimiters.js
@@ -1,14 +1,35 @@
 export default {
+	
+	// Basic Information
 	name: 'unescape_delimiters',
+	author: 'Jesse Traynham',
+	category: 'Encoding',
 	description: 'Unescapes TextSynth tag delimiters.',
-	usage: '[unescape_delimiters: property.path]',
+	kind: 'single',
+	syntax: '[unescape_delimiters: property.path]',
+	version: '1.0.0',
+
+	// Content details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'The path or content to unescape delimiters.'
+		}
+	],
+
+	// Examples for usage
 	examples: [
 		{
-			code: `[unescape_delimiters "[uppercase 'boom']"]`,
-			result: 'Magic'
+			payload: `{ text: '& lbrack;uppercase "boom"& rbrack;' }`,
+			input: '[unescape_delimiters: text]',
+			output: '&lbrack;uppercase "boom"&rbrack;',
+			note: 'Escaped delimiters are converted back to their original form.'
 		},
 	],
-	category: 'Encoding',
+
+	// Processor logic
 	processor(req) {
 		
 		let { raw, esc } = req.textMerger.delimiters
@@ -16,6 +37,7 @@ export default {
 		return req.content
 			.split(esc.start).join(raw.start)
 			.split(esc.end).join(raw.end)
-		
+			
 	}
+	
 }
