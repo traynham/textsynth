bf4fe6057c7cacb5a99f7ae2341d8d06874a3dee
traynham <jesse@traynham.com>
2023-07-13 16:26:33 -0500

    Updated metadata.

--- src/plugins/array_and_text/reverse.js
+++ src/plugins/array_and_text/reverse.js
@@ -1,10 +1,41 @@
 export default {
+	
+	// Basic Information
 	name: 'reverse',
+	author: 'Jesse Traynham',
+	category: 'Array/Text',
 	description: 'Reverses a string or the order of elements in an array.',
-	example: '{{reverse: property.path}}',
-	usage: '{{reverse: site.title}}',
-	category: 'Text',
-	type: ['array', 'string'],
+	kind: 'single',
+	syntax: '[reverse: property.path]',
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'any',
+			required: true,
+			description: 'The input value, which should be a string or an array.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			payload: "{ title: 'Hello World' }",
+			input: '[reverse: title]',
+			output: 'dlroW olleH',
+			note: 'The string is reversed.'
+		},
+		{
+			payload: "{ numbers: [1, 2, 3, 4, 5] }",
+			input: '[reverse: numbers]',
+			output: '[5, 4, 3, 2, 1]',
+			note: 'The order of elements in the array is reversed.'
+		}
+	],
+
+	// Processor logic
 	processor(req) {
 		let {content} = req
 		return Array.isArray(content) ? content.reverse() : content.split('').reverse().join('')	
