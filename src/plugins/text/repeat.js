c12c43fde805b38271961054baa9d37b30604d1a
traynham <jesse@traynham.com>
2023-07-13 16:33:47 -0500

    Updated metadata.

--- src/plugins/text/repeat.js
+++ src/plugins/text/repeat.js
@@ -1,10 +1,47 @@
 export default {
+	
+	// Basic Information
 	name: 'repeat',
-	description: 'Repeats a string a specified number of times.',
-	example: '{{repeat(3): property.path}}',
-	usage: '{{repeat(3): site.title}}',
+	author: 'Jesse Traynham',
 	category: 'Text',
+	description: 'Repeats a string a specified number of times.',
 	kind: 'single',
+	syntax: '[repeat(times): property.path]',
+	version: '1.0.0',
+
+	// Content and Params details
+	content:[
+		{
+			name: 'property.path',
+			type: 'string',
+			required: true,
+			description: 'The path of the property to be repeated.'
+		}
+	],
+	params: [
+		{
+			name: 'times',
+			type: 'number',
+			required: true,
+			description: 'The number of times to repeat the string.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			input: "[repeat(3): 'Hello']",
+			output: "HelloHelloHello",
+			note: 'This example repeats the string "Hello" 3 times.'
+		},
+		{
+			payload: { site: { title: 'My Cool Site' } },
+			input: "[repeat(2): site.title]",
+			output: "My Cool SiteMy Cool Site",
+			note: 'This example repeats the site title 2 times.'
+		}
+	],
+
 	processor(req) {
 		return req.content.repeat(parseInt(req.params[0]))
 	}
