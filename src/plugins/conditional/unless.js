2dd8a5acb76e7a83a1dcd58193cdf02758ef1008
traynham <jesse@traynham.com>
2023-07-13 16:28:00 -0500

    Updated metadata.

--- src/plugins/conditional/unless.js
+++ src/plugins/conditional/unless.js
@@ -1,38 +1,57 @@
 export default {
 
+	// Basic Information
 	name: 'unless',
-
+	author: 'Jesse Traynham',
+	category: 'Conditional',
 	description: 'Renders the content only if the condition is falsy. Useful for displaying a specific message or content when a certain condition is not met.',
+	kind: 'container',
+	syntax: '[unless(condition)] Your content here [/unless]',
+	version: '1.0.0',
 
-	examples: [
+	// Content and Params details
+	content: [
 		{
-			code: "[unless(isAdmin)]You're not an admin.[/unless]",
-			result: "You're not an admin."
-		},
-		{
-			code: "[unless(hasPremiumSubscription)]Upgrade to Premium for more features.[/unless]",
-			result: "Upgrade to Premium for more features."
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'Content to be displayed when the condition is not met.'
 		}
 	],
-	
-	syntax: "[unless(condition)]\nYour content here\n[/unless]",
+	params: [
+		{
+			name: 'condition',
+			type: 'boolean',
+			required: true,
+			description: 'Boolean condition for content rendering.'
+		}
+	],
 
-	category: 'Conditional',
-	
-	kind: 'container',
-	
+	// Examples for usage
+	examples: [
+		{
+			payload: "{ isAdmin: false }",
+			input: "[unless(isAdmin)] You're not an admin. [/unless]",
+			output: "You're not an admin.",
+			note: 'Content is shown as the condition evaluates to false (user is not an admin).'
+		},
+		{
+			payload: "{ hasPremiumSubscription: false }",
+			input: "[unless(hasPremiumSubscription)] Upgrade to Premium for more features. [/unless]",
+			output: "Upgrade to Premium for more features.",
+			note: 'Content is shown as the condition evaluates to false (user does not have a Premium subscription).'
+		},
+	],
+
+	// Processor logic
 	processor(req) {
-		
+
 		// Check if all provided boolean values are true
 		const allTrue = req.params.every((item) => Boolean(item))
-		
+
 		// Return content if all values are false, otherwise return an empty string
-		if (!allTrue) {
-			return req.content
-		}
-		
-		return ''
-		
+		return !allTrue ? req.content : ''
+
 	}
 
 }
