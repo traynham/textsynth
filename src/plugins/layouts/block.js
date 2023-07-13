7b352eeedba2c81b20521a7bd2ba3a59dc639dd6
traynham <jesse@traynham.com>
2023-07-13 16:32:30 -0500

    Updated metadata.

--- src/plugins/layouts/block.js
+++ src/plugins/layouts/block.js
@@ -1,22 +1,44 @@
 export default {
+	
+	// Basic Information
 	name: 'block',
-	kind: 'container',
-	description: 'Defines a block that can be filled later',
+	author: 'Jesse Traynham',
 	category: 'Layout',
-	//usage: '[block \'blockName\']...content...[/block]',
-	example: '[block \'header\']Welcome to our site![/block]',
-	/*
-	processor: (request) => {
-		
-		// PROCESSING IS DONE IN THE LAYOUT PLUGIN.
-		return ''
-		
-		
-		// console.log('IN BLOCK')
-		// const blockName = request.params[0];
-		// if (!(blockName in request.payload)) {
-		// 	request.payload[blockName] = request.content;
-		// }
-	},
-	*/
-};
+	description: 'Defines a block that can be filled later.',
+	kind: 'container',
+	syntax: "[block 'blockName']...content...[/block]",
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: false,
+			description: 'The default content for the block.'
+		}
+	],
+	params: [
+		{
+			name: 'blockName',
+			type: 'string',
+			required: true,
+			description: 'The name of the block.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			input: "[block 'header']Default Header Content[/block]",
+			output: 'The output depends on how the block is later used in the layout.',
+			note: 'This block named "header" can be filled later in a layout.'
+		}
+	]
+
+	// PROCESSING LOGIC IS COVERED IN THE LAYOUT PLUGIN.
+
+	// The basic block tag does not need to do any processing on its own,
+	// it's more about how it's used in a layout plugin.
+
+}
