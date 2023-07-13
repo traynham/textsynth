366df0a52ff0e1cb9abb527d93e34a45af03c4a2
traynham <jesse@traynham.com>
2023-07-13 16:26:28 -0500

    Updated metadata.

--- src/plugins/array_and_text/random.js
+++ src/plugins/array_and_text/random.js
@@ -1,22 +1,39 @@
 export default {
+
+	// Basic Information
 	name: 'random',
+	author: 'Your Name', // Replace with your name
+	category: 'Array/Text',
 	description: 'Selects a random element from an array or a random character from a string.',
-	_example: '{{random: property.path}}',
-	
+	version: '1.0.0',
+	syntax: '[random: property.path]',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'any',
+			required: true,
+			description: 'The input value, which should be an array or a string.'
+		}
+	],
+
+	// Examples for usage
 	examples: [
 		{
-			code: "[random fruits]",
-			result: 'Dragon // Assuming fruits is an array and also happens to include "dragon"'
+			payload: "{ fruits: ['Apple', 'Banana', 'Dragon'] }",
+			input: '[random: fruits]',
+			output: 'Dragon', // Output can be any of the array elements
+			note: 'Output can be any of the array elements, as it is selected randomly.'
 		},
 		{
-			code: "[random 'What letter will it pick?']",
-			result: 't'
+			input: "[random: 'What letter will it pick?']",
+			output: 't', // Output can be any of the string characters
+			note: 'Output can be any of the string characters, as it is selected randomly.'
 		},
 	],
-	
-	usage: '[random site.categories]',
-	category: 'Array/Text',
-	type: ['string', 'array'],
+
+	// Processor logic
 	processor(req) {
 		
 		let content = req.content
@@ -25,6 +42,5 @@ export default {
 		
 		const index = Math.floor(Math.random() * content.length)
 		return content[index]
-		
 	}
 }
