0b6ca002080b00aec695424d6d1493ae8c7994b5
traynham <jesse@traynham.com>
2023-07-13 16:31:36 -0500

    Updated metadata.

--- src/plugins/html/div.js
+++ src/plugins/html/div.js
@@ -1,15 +1,50 @@
 export default {
+	
+	// Basic Information
 	name: 'div',
-	description: 'Generates a div tag with optional attributes, classes and id. All the additional arguments will be treated as HTML attributes and added to the div tag.',
-	examples: [
-		'[div: class="myClass" id="myId" data-custom="customValue"]This is the div content[/div]',
-		'[div: .myClass #myId]This is the div content[/div]'
-	],
-	syntax: '[div: attribute="value" .class #id]content[/div]',
+	author: 'Jesse Traynham',
 	category: 'HTML',
+	description: 'Generates a div tag with optional attributes, classes and id. All the additional arguments will be treated as HTML attributes and added to the div tag.',
 	kind: 'container',
+	syntax: '[div: attribute="value" .class #id]content[/div]',
+	version: '1.0.0',
+	
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'The inner HTML content for the div element.'
+		}
+	],
+	params: [
+		{
+			name: 'params',
+			type: 'string',
+			required: false,
+			description: 'Optional HTML attributes for the div element.'
+		}
+	],
+	
+	// Examples for usage
+	examples: [
+		{
+			payload: "{ }",
+			input: '[div: width="500" #myId data-custom="customValue"]This is the div content[/div]',
+			output: '<div width="500" id="myId" data-custom="customValue">This is the div content</div>',
+			note: 'Generates a div tag with the provided attributes and content.'
+		},
+		{
+			payload: "{ }",
+			input: '[div: .myClass #myId]This is the div content[/div]',
+			output: '<div class="myClass" id="myId">This is the div content</div>',
+			note: 'Generates a div tag with the provided classes, id, and content.'
+		},
+	],
+	
 	processor(req) {
-
+	
 		let attr = {...req.cargo.attributes}
 		
 		// CLASSES
@@ -17,12 +52,12 @@ export default {
 		
 		// ID
 		if(req.cargo.id){ attr.id = req.cargo.id }
-
+		
 		const attributeString = [
 			'',
 			...Object.entries(attr).map(([key, value]) => `${key}="${value}"`),
 			...req.cargo.values
-		].join(' ');
+		].join(' ')
 		
 		let out = `<div${attributeString}>${req.content}</div>`
 		
