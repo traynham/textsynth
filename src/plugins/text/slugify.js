efc0a2fc8148f680d19ed2de277ce375b8d777bf
traynham <jesse@traynham.com>
2023-07-13 16:33:59 -0500

    Updated metadata.

--- src/plugins/text/slugify.js
+++ src/plugins/text/slugify.js
@@ -1,9 +1,39 @@
 export default {
+	
+	// Basic Information
 	name: 'slugify',
-	description: 'Converts a string into a URL-friendly slug.',
-	example: '{{slugify: property.path}}',
-	usage: '{{slugify: property.path}}',
+	author: 'Jesse Traynham',
 	category: 'Text',
+	description: 'Converts a string into a URL-friendly slug.',
+	kind: 'single',
+	syntax: '[slugify: property.path]',
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'property.path',
+			type: 'string',
+			required: true,
+			description: 'The path of the property to be slugified.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			input: "[slugify: 'Hello World!']",
+			output: "hello-world",
+			note: 'This example slugifies the string "Hello World!".'
+		},
+		{
+			payload: { site: { title: 'My Cool Site' } },
+			input: "[slugify: site.title]",
+			output: "my-cool-site",
+			note: 'This example slugifies the site title.'
+		}
+	],
+
 	processor({content}) {
 		if(typeof content !== 'string') return content
 		return content
@@ -12,4 +42,5 @@ export default {
 			.replace(/[\s_-]+/g, '-')
 			.replace(/^-+|-+$/g, '');
 	}
+
 }
