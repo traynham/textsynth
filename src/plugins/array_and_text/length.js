8da929fd92d0518a62b45fb50da96318db502a37
traynham <jesse@traynham.com>
2023-07-13 16:26:21 -0500

    Updated metadata.

--- src/plugins/array_and_text/length.js
+++ src/plugins/array_and_text/length.js
@@ -1,23 +1,42 @@
-const plugin = {
+export default {
+	
+	// Basic Information
 	name: 'length',
+	author: 'Jesse Traynham',
 	description: 'Returns the length of a string or the number of elements in an array.',
-	_example: '{{length: property.path}}',
-	examples: [
+	version: '1.0.0',
+	category: 'Array/Text',
+	kind: 'single',
+	syntax: '[length: property.path]',
+
+	// Content and Params details
+	content: [
 		{
-			code: "[length 'This is 10']",
-			result: '10'
-		},
-		{
-			code: "[length found_array]",
-			result: '15 (Assuming there are 15 array items)'
+			name: 'content',
+			type: ['string', 'array'],
+			required: true,
+			description: 'Content (string or array) whose length is to be determined.'
 		}
 	],
-	usage: '[length items]',
-	category: 'Array/Text',
-	type: ['string', 'array'],
+
+	// Examples for usage
+	examples: [
+		{
+			payload: "{ text: 'This is 10' }",
+			input: '[length: text]',
+			output: '10',
+			note: 'The content is a string of length 10.'
+		},
+		{
+			payload: "{ array: ['one', 'two', 'three', 'four', 'five'] }",
+			input: '[length: array]',
+			output: '5',
+			note: 'The content is an array of length 5.'
+		},
+	],
+
+	// Processor logic
 	processor(req) {
 		return req.content.length;
 	}
 }
-
-export default plugin
