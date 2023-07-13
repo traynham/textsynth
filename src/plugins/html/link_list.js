a435345915f71c37274bac46b72d076f97a28ced
traynham <jesse@traynham.com>
2023-07-13 16:31:43 -0500

    Updated metadata.

--- src/plugins/html/link_list.js
+++ src/plugins/html/link_list.js
@@ -1,10 +1,40 @@
 export default {
+
+	// Basic Information
 	name: 'linkList',
-	description: 'Generates an HTML list of links from an object with name-url pairs.',
-	example: '[linkList: property.path]',
-	usage: '[linkList: site.sidebar]',
+	author: 'Jesse Traynham',
 	category: 'HTML',
+	description: 'Generates an HTML list of links from an object with name-url pairs.',
 	type: ['object'],
+	version: '1.0.0',
+	
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'object',
+			required: true,
+			description: 'An object containing key-value pairs of names and URLs.'
+		}
+	],
+	params: [],
+	
+	// Examples for usage
+	examples: [
+		{
+			payload: {
+				"sidebar": {
+					"Home": "/",
+					"About": "/about",
+					"Contact": "/contact"
+				}
+			},
+			input: '[linkList: sidebar]',
+			output: '<ul><li><a href="/">Home</a></li><li><a href="/about">About</a></li><li><a href="/contact">Contact</a></li></ul>',
+			note: 'Generates an unordered list of links using the names and URLs from the "sidebar" object.'
+		},
+	],
+
 	processor(req) {
 		
 		const listItems = Object.entries(req.content)
