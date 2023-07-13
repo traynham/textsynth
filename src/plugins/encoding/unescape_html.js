aea4487c70370fdadc150d46e5cf737d96d52ab1
traynham <jesse@traynham.com>
2023-07-13 16:31:09 -0500

    Renamed from unescapehtml to unescape_html for consistency

--- src/plugins/encoding/unescape_html.js
+++ src/plugins/encoding/unescape_html.js
@@ -1,9 +1,32 @@
 export default {
-	name: 'unescapeHtml',
+	
+	// Basic Information
+	name: 'unescape_html',
+	author: 'Jesse Traynham',
+	category: 'Encoding',
 	description: 'Unescapes HTML entities in a string, converting them back to their original characters.',
-	example: '{{unescapeHtml: property.path}}',
-	usage: '{{unescapeHtml: property.path}}',
-	category: 'HTML',
+	version: '1.0.0',
+	
+	// Content details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'The string with escaped HTML entities.'
+		}
+	],
+	
+	// Examples for usage
+	examples: [
+		{
+			payload: `{ text: "The &quot;quick&quot; brown fox jumps over the &lt;lazy&gt; dog."}`,
+			input: '[unescape_html: text]',
+			output: 'The "quick" brown fox jumps over the <lazy> dog.',
+			note: 'Converts escaped HTML entities back to their original characters.'
+		},
+	],
+	
 	processor(req) {
 		
 		let escapeCodes = {
