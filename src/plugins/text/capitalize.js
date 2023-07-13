45bcd613aeb9bc413880a053b819023e1307de6b
traynham <jesse@traynham.com>
2023-07-13 16:33:33 -0500

    Updated metadata.

--- src/plugins/text/capitalize.js
+++ src/plugins/text/capitalize.js
@@ -1,9 +1,35 @@
 export default {
+	
+	// Basic Information
 	name: 'capitalize',
-	description: 'Capitalizes the first letter of a string.',
-	example: '{{capitalize: property.path}}',
+	author: 'Jesse Traynham',
 	category: 'Text',
+	description: 'Capitalizes the first letter of a string.',
+	kind: 'single',
+	syntax: '[capitalize: property.path]',
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'The string to be capitalized.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			input: "[capitalize: 'hello world']",
+			output: "Hello world",
+			note: 'This example capitalizes the first letter of the string "hello world".'
+		}
+	],
+
 	processor({content}) {
 		return content.charAt(0).toUpperCase() + content.slice(1);
 	}
+	
 }
