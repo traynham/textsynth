06297d5437499ceda84a8cb84568382d539f1e0a
traynham <jesse@traynham.com>
2023-07-13 16:31:51 -0500

    Updated metadata.

--- src/plugins/iteration/each.js
+++ src/plugins/iteration/each.js
@@ -1,14 +1,57 @@
 export default {
+	
+	// Basic Information
 	name: 'each',
-	description: 'Iterate over an array or an object.',
-	example: `
-		[each: user.hobbies]
-			- [value]
-		[/each]
-	`,
-	usage: '[each: items]\n\tYour content here\n[/each]',
+	author: 'Jesse Traynham',
 	category: 'Iteration',
+	description: 'Iterate over an array or an object.',
 	kind: 'container',
+	syntax: '[each: items]\n\tYour content here\n[/each]',
+	version: '1.0.0',
+	
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'The template to process for each iteration.'
+		}
+	],
+	params: [
+		{
+			name: 'items',
+			type: ['array', 'object'],
+			required: true,
+			description: 'The array or object to iterate over.'
+		}
+	],
+	
+	// Examples for usage
+	examples: [
+		{
+			payload: {
+				"user": {
+					"hobbies": ["Reading", "Coding", "Music"]
+				}
+			},
+			input: '[each: user.hobbies]\n\t- [value]\n[/each]',
+			output: '\\t- Reading\\n\\t- Coding\\n\\t- Music',
+			note: 'Iterates over the array "user.hobbies" and prints each value.'
+		},
+		{
+			payload: {
+				"items": {
+					"name": "John Doe",
+					"age": 30
+				}
+			},
+			input: '[each: items]\n\t[key]: [value]\n[/each]',
+			output: '\\tname: John Doe\\n\\tage: 30',
+			note: 'Iterates over the object "items" and prints each key-value pair.'
+		}
+	],
+
 	processor(req) {
 		
 		let payload = {...req.payload}
