2c1df05e2c3dbae72b65329c530d5e088688d3ca
traynham <jesse@traynham.com>
2023-07-13 16:33:41 -0500

    Updated metadata.

--- src/plugins/text/lowercase.js
+++ src/plugins/text/lowercase.js
@@ -1,14 +1,43 @@
-const lowercase = {
+export default {
+	
+	// Basic Information
 	name: 'lowercase',
-	description: 'Converts a string to lowercase.',
-	example: '{{lowercase: property.path}}',
-	usage: '{{lowercase: site.title}}',
+	author: 'Jesse Traynham',
 	category: 'Text',
+	description: 'Converts a string to lowercase.',
+	kind: 'single',
+	syntax: '[lowercase: property.path]',
 	aliases: ['lower', 'lc'],
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
+		{
+			name: 'property.path',
+			type: 'string',
+			required: true,
+			description: 'The path of the property to be converted to lowercase.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			input: "[lowercase: 'Hello World']",
+			output: "hello world",
+			note: 'This example converts the string "Hello World" to lowercase.'
+		},
+		{
+			payload: { site: { title: 'My Cool Site' } },
+			input: "[lowercase: site.title]",
+			output: "my cool site",
+			note: 'This example converts the site title to lowercase.'
+		}
+	],
+
 	processor({content}) {
 		if(typeof content !== 'string') return content
 		return content.toLowerCase()
 	}
+	
 }
-
-export default lowercase
