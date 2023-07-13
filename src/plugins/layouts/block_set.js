aad51c62fb6639da8fde74bfed92eb5133eb0ebd
traynham <jesse@traynham.com>
2023-07-13 16:32:24 -0500

    Updated metadata.

--- src/plugins/layouts/block_set.js
+++ src/plugins/layouts/block_set.js
@@ -1,34 +1,45 @@
 export default {
+	
+	// Basic Information
 	name: 'block_set',
-	kind: 'container',
-	description: 'Sets the content of a block',
+	author: 'Jesse Traynham',
 	category: 'Layout',
-	usage: "[block_set 'blockName']...content...[/block_set]",
-	example: "[block_set 'header']Welcome to our site![/block_set]",
-	examples: [
+	description: 'Sets (replaces) the content of a block.',
+	kind: 'container',
+	syntax: "[block_set 'blockName']...content...[/block_set]",
+	version: '1.0.0',
+
+	// Content and Params details
+	content: [
 		{
-			code: "[block_set 'header']Welcome to our site![/block_set]",
-			result: "Welcome to our site!"
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'The content to set for the block.'
 		}
 	],
-	/*
-	processor: (req) => {
-		
-		// PROCESSING IS DONE IN THE LAYOUT PLUGIN.
-		return ''
-		
-		// console.log('IN BLOCK SET')
-		// // console.log(
-		// // 	'Hello and Merry Christmas::',
-		// // 	req
-		// // )
-		// 
-		// const blockName = req.params[0];
-		// 
-		// req.payload[blockName] = req.content;
-		// 
-		// return '';
-		
-	},
-	*/
-};
+	params: [
+		{
+			name: 'blockName',
+			type: 'string',
+			required: true,
+			description: 'The name of the block.'
+		}
+	],
+
+	// Examples for usage
+	examples: [
+		{
+			input: "[block_set 'header']Welcome to our site![/block_set]",
+			output: 'The output depends on the initial content of the block in the layout.',
+			note: 'This example sets the content of a block named "header".'
+		}
+	]
+	
+	// PROCESSING LOGIC IS COVERED IN THE LAYOUT PLUGIN.
+
+	// The block_set tag doesn't need to do any processing on its own,
+	// it's about how it's used in a layout plugin.
+	// The actual setting operation is handled by the layout plugin.
+
+}
