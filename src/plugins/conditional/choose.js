551be327729d01169918b287be649689ea596d2c
traynham <jesse@traynham.com>
2023-07-13 16:26:53 -0500

    Updated metadata.

--- src/plugins/conditional/choose.js
+++ src/plugins/conditional/choose.js
@@ -1,35 +1,14 @@
 export default {
 
+	// Basic Information
 	name: 'choose',
-	
-	aliases: 'contingent',
-
+	category: 'Conditional',
 	description: 'Selects one of three possible values based on a boolean condition. If the condition is true, the first value is selected. If false, the second value is selected. If the condition is neither true nor false (i.e., it is null or undefined), the third value is returned.',
-
-	examples: [
-		{
-			code: "[choose(isAdmin) 'Hello, admin!', 'Hello, user!']",
-			result: 'The result depends on the boolean value of isAdmin. If isAdmin is true, the result is "Hello, admin!". Otherwise, the result is "Hello, user!".'
-		},
-		{
-			code: "[choose(hasDiscount) 'You have a discount!', 'No discounts available.', 'Please login to check for discounts.']",
-			result: 'The result depends on the boolean value of hasDiscount. If hasDiscount is true, the result is "You have a discount!". If false, the result is "No discounts available.". If hasDiscount is null or undefined, the result is "Please login to check for discounts.".'
-		}
-	],
-	
 	kind: 'single',
-	
 	syntax: '[choose(condition) trueCase, falseCase, defaultCase]',
+	version: '1.0.0',
 
-	params: [
-		{
-			name: 'condition',
-			type: 'boolean',
-			description: 'The condition to evaluate.',
-			required: true
-		}
-	],
-	
+	// Content and Params details
 	content: [
 		{
 			name: 'trueCase',
@@ -50,22 +29,41 @@ export default {
 			required: false
 		}
 	],
+	params: [
+		{
+			name: 'condition',
+			type: 'boolean',
+			description: 'The condition to evaluate.',
+			required: true
+		}
+	],
+	
+	// Examples for usage
+	examples: [
+		{
+			payload: "{ isAdmin: true }",
+			code: "[choose(isAdmin) 'Hello, admin!', 'Hello, user!']",
+			result: 'The result depends on the boolean value of isAdmin. If isAdmin is true, the result is "Hello, admin!". Otherwise, the result is "Hello, user!".'
+		},
+		{
+			payload: "{ hasDiscount: false }",
+			code: "[choose(hasDiscount) 'You have a discount!', 'No discounts available.', 'Please login to check for discounts.']",
+			result: 'The result depends on the boolean value of hasDiscount. If hasDiscount is true, the result is "You have a discount!". If false, the result is "No discounts available.". If hasDiscount is null or undefined, the result is "Please login to check for discounts.".'
+		}
+	],
 
-	category: 'Conditional',
-
+	// Processor logic
 	processor(req) {
 		
 		let { content, params } = req
 		
 		// IF TRUE
 		if(params[0] == true){
-			//return content[0]
 			return content.params[0]
 		}
 		
 		// IF FALSE
 		if(params[0] == false){
-			//return content[1]
 			return content.params[1]
 		}
 		
