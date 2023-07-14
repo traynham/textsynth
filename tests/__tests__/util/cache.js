486324010e4ab60b11dd9ecbff149b64c55f62b2
traynham <jesse@traynham.com>
2023-07-14 16:12:16 -0500

    Added/fixed some tests and data.

--- tests/__tests__/util/cache.js
+++ tests/__tests__/util/cache.js
@@ -2,13 +2,10 @@ import { describe, expect, test, jest } from '@jest/globals'
 import TextSynth from '../index.js'
 import { randomUUID } from 'crypto'
 
-//import fs from 'fs'
-//import path from 'path'
 
 jest.useRealTimers() // Ensure we're using real timers
 
 const textSynth = await TextSynth()
-//textSynth.setDelimiters({opener: '[', closer: ']'})
 
 textSynth.pluginSettings(
 	'cache', 
@@ -39,12 +36,6 @@ const template2 = `
 
 
 describe('cache plugin', () => {
-	
-	// afterEach(() => {
-	// 	// Clean up cache files after each test
-	// 	const cacheFolder = './cache'
-	// 	fs.rmdirSync(cacheFolder, { recursive: true })
-	// })
 
 	test('caches the content and serves it if not expired', async () => {
 		const input = template
@@ -62,20 +53,4 @@ describe('cache plugin', () => {
 		expect(secondResult.trim()).toBe('<h2>Header 2</h2>')
 	})
 	
-	
-/*
-	test('reprocesses the content if cache is expired', async () => {
-		const input = '[cache: "testCache2", "1s"]Cache me, man.[/cache]'
-		
-		const firstResult = textSynth.merge(input, {})
-		
-		await new Promise(resolve => setTimeout(resolve, 1500)) // Wait for cache to expire
-		
-		const secondResult = textSynth.merge(input, {})
-		
-//		expect(firstResult).not.toBe(secondResult)
-		expect(firstResult).toBe(secondResult)
-		
-	})
-*/
 })
