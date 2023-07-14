f410952b93c02856948c07469332e468d53fcc8f
traynham <jesse@traynham.com>
2023-07-14 16:06:14 -0500

    Added/fixed some tests and data.

--- tests/__tests__/array_and_text/slice.js
+++ tests/__tests__/array_and_text/slice.js
@@ -8,7 +8,8 @@ const payload = {
 	site: {
 		title: 'Hello World'
 	},
-	numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+	empty: []
 }
 
 // Turning off console.
@@ -21,28 +22,29 @@ describe('slice plugin', () => {
 		expect(textSynth.merge(input, payload)).toBe('Hello')
 	})
 
-	// test('extracts a section of an array', () => {
-	// 	const input = "[slice(4,9) numbers]"
-	// 	expect(textSynth.merge(input, payload)).toEqual([5, 6, 7, 8, 9])
-	// })
+	test('extracts a section of an array', () => {
+		const input = "[slice(4,9) numbers]"
+		expect(textSynth.merge(input, payload)).toEqual('5,6,7,8,9')
+	})
 
-	// test('returns an empty string when the string is empty', () => {
-	// 	const input = "[slice(0,2) '']"
-	// 	expect(textSynth.merge(input, payload)).toBe('')
-	// })
+	test('returns an empty string when the string is empty', () => {
+		const input = "[slice(0,2) '']"
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
 
-	// test('returns an empty array when the array is empty', () => {
-	// 	const input = "[slice(0,2) []]"
-	// 	expect(textSynth.merge(input, payload)).toEqual([])
-	// })
+	test('returns an empty array when the array is empty', () => {
+		const input = "[slice(0,2) empty]"
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
 
-	// test('returns full array when end index is larger than array length', () => {
-	// 	const input = "[slice(0,20) numbers]"
-	// 	expect(textSynth.merge(input, payload)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-	// })
+	test('returns full array when end index is larger than array length', () => {
+		const input = "[slice(0,20) numbers]"
+		expect(textSynth.merge(input, payload)).toEqual('1,2,3,4,5,6,7,8,9,10')
+	})
 
 	test('returns full string when end index is larger than string length', () => {
 		const input = "[slice(0,20) site.title]"
 		expect(textSynth.merge(input, payload)).toBe('Hello World')
 	})
+	
 })
