bb7fa42f56fd67a02f25857bcf62f83e1d67803b
traynham <jesse@traynham.com>
2023-07-14 16:10:36 -0500

    Added/fixed some tests and data.

--- tests/__tests__/html/div.js
+++ tests/__tests__/html/div.js
@@ -3,20 +3,21 @@ import TextSynth from '../index.js'
 
 const textSynth = await TextSynth()
 
+// Turning off console.
+console.log = () => {}
+
 describe('div plugin', () => {
-/*	
+
 	test('generates div tag with classes, id and attributes', () => {
-		//const input = '[div: class="myClass" id="myId" datacustom="customValue"]This is the div content[/div]';
-		const input = '[div: .myClass #myId datacustom="customValue"]This is the div content[/div]';
-		//const expectedOutput = '<div class="myClass" id="myId" data-custom="customValue">This is the div content</div>';
-		const expectedOutput = '<div datacustom="customValue" class="myClass" id="myId">This is the div content</div>';
-		console.log('RESULT::', textSynth.merge(input, {}) )
-		expect(textSynth.merge(input, {})).toBe(expectedOutput);
-	});
-*/
+		const input = '[div: .myClass #myId data-custom="customValue" name="content"]This is the div content[/div]'
+		const expectedOutput = '<div name="content" class="myClass" id="myId" data-custom="customValue">This is the div content</div>'
+		expect(textSynth.merge(input, {})).toBe(expectedOutput)
+	})
+
 	test('generates div tag with classes and id shorthand', () => {
-		const input = '[div: .myClass #myId]This is the div content[/div]';
-		const expectedOutput = '<div class="myClass" id="myId">This is the div content</div>';
-		expect(textSynth.merge(input, {})).toBe(expectedOutput);
-	});
-});
+		const input = '[div: .myClass #myId]This is the div content[/div]'
+		const expectedOutput = '<div class="myClass" id="myId">This is the div content</div>'
+		expect(textSynth.merge(input, {})).toBe(expectedOutput)
+	})
+	
+})
