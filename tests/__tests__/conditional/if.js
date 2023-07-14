52af83e49ffcd5ccfa7ab96b3c1465f26bb2ca3e
traynham <jesse@traynham.com>
2023-07-14 16:06:36 -0500

    Updated tag delimiters to default.

--- tests/__tests__/conditional/if.js
+++ tests/__tests__/conditional/if.js
@@ -2,7 +2,6 @@ import { describe, expect, test } from '@jest/globals';
 import TextSynth from '../index.js';
 
 const textSynth = await TextSynth()
-textSynth.setDelimiters({opener: '{{', closer: '}}'})
 
 // !TURN OFF CONSOLE.
 console.log = () => {}
@@ -18,32 +17,52 @@ const payload = {
 describe('If plugin', () => {
 
 	test('should show content on true', () => {
-		const input = '{{if: user.isAdmin}}Hello Admin{{/if}}'
+		const input = '[if: user.isAdmin]Hello Admin[/if]'
 		expect(textSynth.merge(input, payload)).toBe('Hello Admin')
 	})
 	
 	test('should hide content on false', () => {
-		const input = '{{if: user.isMod}}Hello Mod{{/if}}'
+		const input = '[if: user.isMod]Hello Mod[/if]'
 		expect(textSynth.merge(input, payload)).toBe('')
 	})
 	
+	test('do not show content on bogus key', () => {
+		const input = '[if: bogus]Hello Admin[/if]'
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
+	
+	test('valid string should resolve as truthy', () => {
+		const input = '[if: "bogus"]Hello Admin[/if]'
+		expect(textSynth.merge(input, payload)).toBe('Hello Admin')
+	})
+	
+	test('a string of "true" should be cast as true.', () => {
+		const input = '[if: "true"]Hello Admin[/if]'
+		expect(textSynth.merge(input, payload)).toBe('Hello Admin')
+	})
+	
+	test('a string of "false" should be cast as false.', () => {
+		const input = '[if: "false"]Hello Admin[/if]'
+		expect(textSynth.merge(input, payload)).toBe('')
+	})
+
 	test('should allow multiple params', () => {
-		const input = '{{if: user.isAdmin, user.isMod}}Hello Mod{{/if}}'
+		const input = '[if: user.isAdmin, user.isMod]Hello Mod[/if]'
 		expect(textSynth.merge(input, payload)).toBe('')
 	})
 	
 	test('should allow literal params', () => {
-		const input = '{{if: true, false}}Hello Mod{{/if}}'
+		const input = '[if: true, false]Hello Mod[/if]'
 		expect(textSynth.merge(input, payload)).toBe('')
 	})
 	
 	test('should allow else', () => {
-		const input = '{{if: false}}Hello Mod{{else}}else me{{/if}}'
+		const input = '[if: false]Hello Mod[else]else me[/if]'
 		expect(textSynth.merge(input, payload)).toBe('else me')
 	})
 	
 	test('should not allow multiple else', () => {
-		const input = '{{if: false}}Hello Mod{{else}}else me{{else}}boom{{/if}}'
+		const input = '[if: false]Hello Mod[else]else me[else]boom[/if]'
 		expect(textSynth.merge(input, payload)).toBe('') // RETURNS EMPTY STRING BECAUSE THROW IS SUPPRESSED IN TEXTSYNTH
 	})
 	
