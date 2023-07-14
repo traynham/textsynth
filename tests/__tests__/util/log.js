b6177a26a3ca6c24c491bc393de851d3213f264e
traynham <jesse@traynham.com>
2023-07-14 16:12:56 -0500

    Added/fixed some tests and data.

--- tests/__tests__/util/log.js
+++ tests/__tests__/util/log.js
@@ -4,28 +4,36 @@ import TextSynth from '../index.js';
 const textSynth = await TextSynth()
 textSynth.setDelimiters({ opener: '{{', closer: '}}' })
 
+// Turning off console.
+console.log = () => {}
+
 describe('log plugin', () => {
 
 	test('logs the correct content to console', () => {
-		const logMock = jest.spyOn(console, 'log');
 		
-		const input = '{{log: "Hello, World!"}}';
+		const logMock = jest.spyOn(console, 'log')
+		
+		const input = '{{log: "Hello, World!"}}'
 		textSynth.merge(input);
-
-		expect(logMock).toHaveBeenCalledWith('Hello, World!');
-
-		logMock.mockRestore();
-	});
+		
+		expect(logMock).toHaveBeenCalledWith('Hello, World!')
+		
+		logMock.mockRestore()
+		
+	})
 
 	test('logs object correctly', () => {
-		const logMock = jest.spyOn(console, 'log');
-
-		const payload = { property: { path: { key: "value" } } };
-		const input = '{{log: property.path}}';
-		textSynth.merge(input, payload);
-
-		expect(logMock).toHaveBeenCalledWith({ key: "value" });
-
-		logMock.mockRestore();
-	});
-});
+		
+		const logMock = jest.spyOn(console, 'log')
+		
+		const payload = { property: { path: { key: "value" } } }
+		const input = '{{log: property.path}}'
+		textSynth.merge(input, payload)
+		
+		expect(logMock).toHaveBeenCalledWith({ key: "value" })
+		
+		logMock.mockRestore()
+		
+	})
+	
+})
