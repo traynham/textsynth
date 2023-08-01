3774a96d1af87a8c4ed4f89b6c10b03b8bc9b34e
traynham <jesse@traynham.com>
2023-08-01 18:06:41 -0500

    Added and updated tests.

--- tests/__tests__/html/html_tag.js
+++ tests/__tests__/html/html_tag.js
@@ -10,13 +10,19 @@ describe('div plugin', () => {
 
 	test('generates div tag with classes, id and attributes', () => {
 		const input = '[div: .myClass #myId data-custom="customValue" name="content"]This is the div content[/div]'
-		const expectedOutput = '<div name="content" class="myClass" id="myId" data-custom="customValue">This is the div content</div>'
+		const expectedOutput = '<div data-custom="customValue" name="content" class="myClass" id="myId">This is the div content</div>'
 		expect(textSynth.merge(input, {})).toBe(expectedOutput)
 	})
 
 	test('generates div tag with classes and id shorthand', () => {
 		const input = '[div: .myClass #myId]This is the div content[/div]'
 		const expectedOutput = '<div class="myClass" id="myId">This is the div content</div>'
+		expect(textSynth.merge(input, {})).toBe(expectedOutput)
+	})
+	
+	test('generates div tag with quoted values', () => {
+		const input = `[div: 'required' selected "Some bad value"]This is the div content[/div]`
+		const expectedOutput = '<div required selected "Some bad value">This is the div content</div>'
 		expect(textSynth.merge(input, {})).toBe(expectedOutput)
 	})
 	
