5e5f085d6ac96f36046314e1b66c8ce2e33a5202
traynham <jesse@traynham.com>
2023-08-23 17:24:36 -0500

    Initial commit. Experimental.

---
+++ src/plugins/formatting/ignore.js
@@ -0,0 +1,39 @@
+export default {
+	
+	// Basic Information
+	name: 'ignore',
+	author: 'Jesse Traynham',
+	category: 'Formatting',
+	aliases: ['md'],
+	description: 'Ignores the content and returns it as a raw string, escaping any special characters.',
+	kind: 'container',
+	syntax: '[ignore] Your raw content here [/ignore]',
+	version: '1.0.0',
+	
+	// Content and Params details
+	content: [
+		{
+			name: 'content',
+			type: 'string',
+			required: true,
+			description: 'Content to be returned as a raw string.'
+		}
+	],
+	
+	// Examples for usage
+	examples: [
+		{
+			payload: "{ }",
+			input: '[ignore]# Header \n **Bold Text** [/ignore]',
+			output: '# Header \n **Bold Text**',
+			note: 'Content is returned as raw text.'
+		},
+	],
+
+	processor(req) {
+		let { esc, raw } = req.textMerger.delimiters
+		let out = req.content?.split(raw.start).join(esc.start).split(raw.end).join(esc.end)
+		return out
+	}
+	
+}
