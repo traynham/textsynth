25426f7af40b6876f67d9e3131e882883a49dac6
traynham <jesse@traynham.com>
2023-08-23 17:26:46 -0500

    Fixed test.

--- tests/__tests__/encoding/stringify.js
+++ tests/__tests__/encoding/stringify.js
@@ -23,7 +23,7 @@ describe('stringify plugin', () => {
 	test('converts a JavaScript value to a JSON string', () => {
 		const input = "[stringify data]"
 		const expectedOutput = JSON.stringify(payload.data)
-		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
+		expect(textSynth.merge(input, payload)).toBe(expectedOutput.replaceAll('[', '\\[').replaceAll(']', '\\]'))
 	})
 
 	test('returns an empty string when the content is null', () => {
