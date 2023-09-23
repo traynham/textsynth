646bdd70150c3f64a8cd26f761fe46f39a80ec6c
traynham <jesse@traynham.com>
2023-09-23 12:29:28 -0500

    Updated tags to include “:”.

--- tests/__tests__/array_and_text/count.js
+++ tests/__tests__/array_and_text/count.js
@@ -19,37 +19,37 @@ console.log = () => {}
 describe('count plugin', () => {
 
 	test('counts the characters in a string', () => {
-		const input = "[count testString]"
+		const input = "[count: testString]"
 		expect(textSynth.merge(input, payload)).toBe('10')
 	})
 
 	test('counts the number of elements in an array', () => {
-		const input = "[count foundArray]"
+		const input = "[count: foundArray]"
 		expect(textSynth.merge(input, payload)).toBe('15')
 	})
 
 	test('counts the occurrences of a specified character in a string!', () => {
-		const input = "[count('t') testStringWithChar]"
+		const input = "[count('t'): testStringWithChar]"
 		expect(textSynth.merge(input, payload)).toBe('2')
 	})
 	
 	test('Object should simply return object.', () => {
-		const input = "[count anObj]"
+		const input = "[count: anObj]"
 		expect(textSynth.merge(input, payload)).toBe('[object Object]')
 	})
 	
 	test('counts the occurrences of a specified q in an array', () => {
-		const input = "[count('a') qarray]"
+		const input = "[count('a'): qarray]"
 		expect(textSynth.merge(input, payload)).toBe('2')
 	})
 
 	test('returns the input as is when it is not a string or array', () => {
-		const input = "[count num]"
+		const input = "[count: num]"
 		expect(textSynth.merge(input, payload)).toBe('123')
 	})
 	
 	test('returns the input as is when it is not a string or array', () => {
-		const input = "[count 123456789]"
+		const input = "[count: 123456789]"
 		expect(textSynth.merge(input, payload)).toBe('123456789')
 	})
 	
