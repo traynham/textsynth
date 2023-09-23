187d01e1fe1c2c710acdb57eb795cbfe4e05cdea
traynham <jesse@traynham.com>
2023-09-23 12:36:53 -0500

    Updated tags to include “:”.

--- tests/__tests__/text/replace.js
+++ tests/__tests__/text/replace.js
@@ -16,22 +16,22 @@ console.log = () => {}
 describe('replace plugin', () => {
 
 	test('replaces all occurrences of a search string with a replacement string', () => {
-		const input = "[replace(' ', '-') testString]"
+		const input = "[replace(' ', '-'): testString]"
 		expect(textSynth.merge(input, payload)).toBe('This-is-a-test')
 	})
 
 	test('handles special characters in the replacement string', () => {
-		const input = "[replace(' ', '!') testString]"
+		const input = "[replace(' ', '!'): testString]"
 		expect(textSynth.merge(input, payload)).toBe('This!is!a!test')
 	})
 
 	test('replaces special characters in the input string', () => {
-		const input = "[replace('!@#', '---') testStringWithSpecialChars]"
+		const input = "[replace('!@#', '---'): testStringWithSpecialChars]"
 		expect(textSynth.merge(input, payload)).toBe('This is a --- test')
 	})
 
 	test('throws an error when replacing a non-string', () => {
-		const input = "[replace(' ', '-') nonString]"
+		const input = "[replace(' ', '-'): nonString]"
 		expect(textSynth.merge(input, payload)).toBe(String(payload.nonString))
 	})
 
