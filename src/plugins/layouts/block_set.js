1a0d0ed3c0e82fcaffbac84a42820f42f404865d
traynham <jesse@traynham.com>
2023-09-23 12:26:19 -0500

    Updated to use _layoutStack.

--- src/plugins/layouts/block_set.js
+++ src/plugins/layouts/block_set.js
@@ -38,11 +38,15 @@ export default {
 	
 	processor({ content, params, payload, textMerger}) {
 		
+		if(!payload._layoutStack){
+			return ''
+		}
+		
 		// EXTRACT THE BLOCK NAME FROM THE PARAMETERS PASSED IN
-		const blockName = params[0]
+		const blockName = params[0].value
 		
 		// ACCESS THE MOST RECENT LAYOUT FROM THE STACK IN TEXTMERGER
-		let currentLayout = textMerger.layoutStack[textMerger.layoutStack.length - 1]
+		let currentLayout = payload._layoutStack[payload._layoutStack.length - 1]
 		
 		// ASSIGN THE PROCESSED CONTENT TO THE BLOCK WITH THE SPECIFIED NAME IN THE CURRENT LAYOUT.
 		// IF THE BLOCK DOES NOT EXIST, IT WILL CREATE A NEW ONE WITH THE PROCESSED CONTENT.
