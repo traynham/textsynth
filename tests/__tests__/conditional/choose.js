41e3e692579a183264981328106685666334dd85
traynham <jesse@traynham.com>
2023-09-23 12:30:18 -0500

    Updated tags to include “:”.

--- tests/__tests__/conditional/choose.js
+++ tests/__tests__/conditional/choose.js
@@ -17,24 +17,25 @@ console.log = () => {}
 describe('choose plugin', () => {
 
 	test('returns first content when condition is true quoted string', () => {
-		const input = `[choose('true') "Hello Admin!", "Hello User!", "Hello"]`
+		const input = `[choose('true'): "Hello Admin!", "Hello User!", "Hello"]`
 		expect(textSynth.merge(input, payload)).toBe('Hello Admin!')
 	})
 	
 	test('returns second content when condition is false quoted string', () => {
-		const input = `[choose('false') "Hello Admin!", "Hello User!", "Hello"]`
+		const input = `[choose('false'): "Hello Admin!", "Hello User!", "Hello"]`
 		expect(textSynth.merge(input, payload)).toBe('Hello User!')
 	})
 	
 	test('returns first content when condition is true string', () => {
-		const input = `[choose(true) "Hello Admin!", "Hello User!", "Hello"]`
+		const input = `[choose(true): "Hello Admin!", "Hello User!", "Hello"]`
 		expect(textSynth.merge(input, payload)).toBe('Hello Admin!')
 	})
 	
-	test('returns first content when condition is true string, using space delimiters', () => {
-		const input = `[choose(true) "Hello Admin!" "Hello User!" "Hello"]`
-		expect(textSynth.merge(input, payload)).toBe('Hello Admin!')
-	})
+	// NO LONGER SUPPORTED
+	// test('returns first content when condition is true string, using space delimiters', () => {
+	// 	const input = `[choose(true): "Hello Admin!" "Hello User!" "Hello"]`
+	// 	expect(textSynth.merge(input, payload)).toBe('Hello Admin!')
+	// })
 	
 	test('returns second content when condition is false string', () => {
 		const input = `[choose(false): "Hello Admin!", "Hello User!", "Hello"]`
@@ -42,7 +43,7 @@ describe('choose plugin', () => {
 	})
 	
 	test('returns first content when condition is true', () => {
-		const input = '[choose(isAdmin) "Hello Admin!", "Hello User!", "Hello"]'
+		const input = '[choose(isAdmin): "Hello Admin!", "Hello User!", "Hello"]'
 		expect(textSynth.merge(input, payload)).toBe('Hello Admin!')
 	})
 	
@@ -52,12 +53,12 @@ describe('choose plugin', () => {
 	})
 	
 	test('returns default content when condition is neither true nor false', () => {
-		const input = '[choose(isUndefined) "Hello Admin!", "Hello User!", "Hello"]'
+		const input = '[choose(isUndefined): "Hello Admin!", "Hello User!", "Hello"]'
 		expect(textSynth.merge(input, payload)).toBe('Hello')
 	})
 	
 	test('returns default content when condition is null', () => {
-		const input = '[choose(isNull) "Hello Admin!", "Hello User!", "Hello"]'
+		const input = '[choose(isNull): "Hello Admin!", "Hello User!", "Hello"]'
 		expect(textSynth.merge(input, payload)).toBe('Hello')
 	})
 	
