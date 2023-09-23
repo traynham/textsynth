46360e9f63c1693830e108bdeaaa8d2a0f4f8f4f
traynham <jesse@traynham.com>
2023-09-23 12:33:48 -0500

    Updated tags to include “:” and other fixes.

--- tests/__tests__/layouts/layout.js
+++ tests/__tests__/layouts/layout.js
@@ -22,8 +22,8 @@ console.log = () => {}
 describe('layout plugin', () => {
 	
 	test('Replaces block placeholders with block content', () => {
-		const input = `[layout "main.synth"] [block_set "title"]Page Title[/block_set] [block_set "body"]Page content[/block_set] [/layout]`
-		const expectedOutput = '<title>Page Title</title>\n<div>Page content</div>'
+		const input = `[layout: "main.synth"] [block_set: "title"]Page Title[/block_set] [block_set: "body"]Page content[/block_set] [/layout]`
+		const expectedOutput = '<head></head>\n<title>Page Title</title>\n<div>Page content</div>'
 		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
 	})
 	
@@ -39,31 +39,38 @@ describe('layout plugin', () => {
 			`,
 		}
 		
-		const input = `---\nblock: "body"\n---\n[block_set "title"]Page Title[/block_set]This is the body`
+		const input = `---\nblock: "body"\n---\n[block_set: "title"]Page Title[/block_set]This is the body`
 		const expectedOutput = '\n# Page Title\nThis is the body\n'
 		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
 	})
 	
 	test('Keeps default content if no block_set is provided', () => {
-		const input = `[layout "main.synth"] [/layout]`
-		const expectedOutput = '<title>Default Title</title>\n<div>Default body</div>'
+		const input = `[layout: "main.synth"] [/layout]`
+		const expectedOutput = '<head></head>\n<title>Default Title</title>\n<div>Default body</div>'
+		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
+	})
+	
+	
+	test('Error issue when layout content is empty.', () => {
+		const input = `[layout: "main.synth"][/layout]`
+		const expectedOutput = '<head></head>\n<title>Default Title</title>\n<div>Default body</div>'
 		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
 	})
 
 	test('Appends content with block_append', () => {
-		const input = `[layout "main.synth"] [block_append "body"]Additional content[/block_append] [/layout]`
-		const expectedOutput = '<title>Default Title</title>\n<div>Default bodyAdditional content</div>'
+		const input = `[layout: "main.synth"][block_append: "body"]Additional content[/block_append][/layout]`
+		const expectedOutput = '<head></head>\n<title>Default Title</title>\n<div>Default bodyAdditional content</div>'
 		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
 	})
 	
 	test('Prepends content with block_prepend', () => {
-		const input = `[layout "main.synth"] [block_prepend "body"]Additional content[/block_prepend] [/layout]`
-		const expectedOutput = '<title>Default Title</title>\n<div>Additional contentDefault body</div>'
+		const input = `[layout: "main.synth"] [block_prepend: "body"]Additional content[/block_prepend] [/layout]`
+		const expectedOutput = '<head></head>\n<title>Default Title</title>\n<div>Additional contentDefault body</div>'
 		expect(textSynth.merge(input, payload)).toBe(expectedOutput)
 	})
 	
 	test('Throws error if layout file not found', () => {
-		const input = `[layout "missing.synth"] [block_set "title"]Page Title[/block_set] [block_set "body"]Page content[/block_set] [/layout]`
+		const input = `[layout: "missing.synth"] [block_set: "title"]Page Title[/block_set] [block_set: "body"]Page content[/block_set] [/layout]`
 		const output = textSynth.merge(input, payload)
 		expect(output).toMatch(/^ERROR: Layout file does not exist:/)
 	})
@@ -73,5 +80,5 @@ describe('layout plugin', () => {
 		const output = textSynth.merge(input, payload)
 		expect(output).toBe('Body Block')
 	})
-	
+
 })
