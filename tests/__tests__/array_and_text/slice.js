105c3877039b429d2ec67e041f3167ea708d12a8
traynham <jesse@traynham.com>
2023-09-23 12:30:06 -0500

    Updated tags to include “:”.

--- tests/__tests__/array_and_text/slice.js
+++ tests/__tests__/array_and_text/slice.js
@@ -18,32 +18,32 @@ console.log = () => {}
 describe('slice plugin', () => {
 
 	test('extracts a section of a string', () => {
-		const input = "[slice(0,5) site.title]"
+		const input = "[slice(0,5): site.title]"
 		expect(textSynth.merge(input, payload)).toBe('Hello')
 	})
 
 	test('extracts a section of an array', () => {
-		const input = "[slice(4,9) numbers]"
+		const input = "[slice(4,9): numbers]"
 		expect(textSynth.merge(input, payload)).toEqual('5,6,7,8,9')
 	})
 
 	test('returns an empty string when the string is empty', () => {
-		const input = "[slice(0,2) '']"
+		const input = "[slice(0,2): '']"
 		expect(textSynth.merge(input, payload)).toBe('')
 	})
 
 	test('returns an empty array when the array is empty', () => {
-		const input = "[slice(0,2) empty]"
+		const input = "[slice(0,2): empty]"
 		expect(textSynth.merge(input, payload)).toBe('')
 	})
 
 	test('returns full array when end index is larger than array length', () => {
-		const input = "[slice(0,20) numbers]"
+		const input = "[slice(0,20): numbers]"
 		expect(textSynth.merge(input, payload)).toEqual('1,2,3,4,5,6,7,8,9,10')
 	})
 
 	test('returns full string when end index is larger than string length', () => {
-		const input = "[slice(0,20) site.title]"
+		const input = "[slice(0,20): site.title]"
 		expect(textSynth.merge(input, payload)).toBe('Hello World')
 	})
 	
