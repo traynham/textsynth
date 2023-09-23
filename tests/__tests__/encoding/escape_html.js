a1893945a4f04eb9ef463f35be665431f9796d79
traynham <jesse@traynham.com>
2023-09-23 12:31:24 -0500

    Updated tags to include “:”.

--- tests/__tests__/encoding/escape_html.js
+++ tests/__tests__/encoding/escape_html.js
@@ -16,22 +16,22 @@ console.log = () => {}
 describe('escape_html plugin', () => {
 
 	test('escapes HTML entities in a string', () => {
-		const input = "[escape_html html]"
+		const input = "[escape_html: html]"
 		expect(textSynth.merge(input, payload)).toBe('&lt;h1&gt;Hello World&lt;/h1&gt;')
 	})
 
 	test('escapes quotes and apostrophes in a string', () => {
-		const input = "[escape_html text]"
+		const input = "[escape_html: text]"
 		expect(textSynth.merge(input, payload)).toBe('Click &apos;OK&apos; to confirm')
 	})
 
 	test('escapes square brackets in a string', () => {
-		const input = "[escape_html array]"
+		const input = "[escape_html: array]"
 		expect(textSynth.merge(input, payload)).toBe('&lbrack;1,2,3&rbrack;')
 	})
 
 	test('returns an empty string when the string is empty', () => {
-		const input = "[escape_html '']"
+		const input = "[escape_html: '']"
 		expect(textSynth.merge(input, payload)).toBe('')
 	})
 	
