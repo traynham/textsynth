d5369ca28be9bc208b0a9abff2c98ad96e9ee58f
traynham <jesse@traynham.com>
2023-09-23 12:37:01 -0500

    Updated tags to include “:”.

--- tests/__tests__/text/slugify.js
+++ tests/__tests__/text/slugify.js
@@ -16,17 +16,17 @@ console.log = () => {}
 describe('slugify plugin', () => {
 
 	test('converts a string into a URL-friendly slug', () => {
-		const input = "[slugify title]"
+		const input = "[slugify: title]"
 		expect(textSynth.merge(input, payload)).toBe('this-is-a-test-title')
 	})
 
 	test('removes special characters from the input string', () => {
-		const input = "[slugify titleWithSpecialChars]"
+		const input = "[slugify: titleWithSpecialChars]"
 		expect(textSynth.merge(input, payload)).toBe('this-is-a-test-title')
 	})
 
 	test('throws an error when slugifying a non-string', () => {
-		const input = "[slugify nonString]"
+		const input = "[slugify: nonString]"
 		expect(textSynth.merge(input, payload)).toBe(`${payload.nonString}`)
 	})
 	
