fff0cefe6645e645e49fc50c918e85369c3c914c
traynham <jesse@traynham.com>
2023-10-14 17:18:41 -0500

    Added tests for new functionality.

--- tests/__tests__/util/log.js
+++ tests/__tests__/util/log.js
@@ -6,11 +6,12 @@ const textSynth = await TextSynth()
 // Turning off console.
 console.log = () => {}
 
+const payload = { property: { path: { key: "value" } } }
+const logMock = jest.spyOn(console, 'log')
+
 describe('log plugin', () => {
 
 	test('logs the correct content to console', () => {
-		
-		const logMock = jest.spyOn(console, 'log')
 		
 		const input = '[log: "Hello, World!"]'
 		textSynth.merge(input);
@@ -25,11 +26,44 @@ describe('log plugin', () => {
 		
 		const logMock = jest.spyOn(console, 'log')
 		
-		const payload = { property: { path: { key: "value" } } }
 		const input = '[log: property.path]'
 		textSynth.merge(input, payload)
 		
 		expect(logMock).toHaveBeenCalledWith({ key: "value" })
+		
+		logMock.mockRestore()
+		
+	})
+	
+	// NOTE: THIS TEST IS BAD.
+	test('logs grouping and property', () => {
+		
+		const logMock = jest.spyOn(console, 'log')
+		
+		const input = '[log: "Simple String", property]'
+		textSynth.merge(input, payload)
+		
+		expect(logMock).toHaveBeenCalledWith({"path": {"key": "value"}})
+		
+		logMock.mockRestore()
+		
+	})
+	
+	// NOTE: THIS TEST IS BAD.
+	test('logs grouping and property', () => {
+		
+		const logMock = jest.spyOn(console, 'log')
+		
+		const input = '[log: "Simple String", property.path, -table]'
+		textSynth.merge(input, payload)
+		
+		const output =  "┌─────────┬─────────┐\n" +
+						"│ (index) │ Values  │\n" +
+						"├─────────┼─────────┤\n" +
+						"│   key   │ 'value' │\n" +
+						"└─────────┴─────────┘"
+		
+		expect(logMock).toHaveBeenCalledWith(output)
 		
 		logMock.mockRestore()
 		
