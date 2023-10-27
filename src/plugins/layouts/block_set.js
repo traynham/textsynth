6eabcc0422a17b61763f66de0d4493277d74f735
traynham <jesse@traynham.com>
2023-10-27 17:55:02 -0500

    Renamed textMerger instances to engine.

--- src/plugins/layouts/block_set.js
+++ src/plugins/layouts/block_set.js
@@ -36,7 +36,7 @@ export default {
 		}
 	],
 	
-	processor({ content, params, payload, textMerger}) {
+	processor({ content, params, payload, engine}) {
 		
 		if(!payload._layoutStack){
 			return ''
@@ -45,12 +45,12 @@ export default {
 		// EXTRACT THE BLOCK NAME FROM THE PARAMETERS PASSED IN
 		const blockName = params[0].value
 		
-		// ACCESS THE MOST RECENT LAYOUT FROM THE STACK IN TEXTMERGER
+		// ACCESS THE MOST RECENT LAYOUT FROM THE STACK
 		let currentLayout = payload._layoutStack[payload._layoutStack.length - 1]
 		
 		// ASSIGN THE PROCESSED CONTENT TO THE BLOCK WITH THE SPECIFIED NAME IN THE CURRENT LAYOUT.
 		// IF THE BLOCK DOES NOT EXIST, IT WILL CREATE A NEW ONE WITH THE PROCESSED CONTENT.
-		currentLayout[blockName] = textMerger.process(content, payload)
+		currentLayout[blockName] = engine.process(content, payload)
 		
 		// AS THIS IS A BLOCK_SET PROCESSOR, IT DOESN'T NEED TO RETURN ANY CONTENT 
 		// FOR THE CURRENT PROCESSING STAGE, SO IT RETURNS AN EMPTY STRING.
