a0a7607e729327394556cce351dba3f4c1988911
traynham <jesse@traynham.com>
2023-10-27 20:07:24 -0500

    Corrected delimiters path due to engine update.

--- src/plugins/encoding/unencode_delimiters.js
+++ src/plugins/encoding/unencode_delimiters.js
@@ -34,7 +34,7 @@ export default {
 		
 		if(!req.content) { return req.content }
 		
-		let { raw, enc } = req.engine.delimiters
+		let { raw, enc } = req.engine.options.delimiters
 		
 		return req.content
 			.split(enc.start).join(raw.start)
