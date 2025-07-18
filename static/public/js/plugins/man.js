66a6f7eec0c30766fafd155f67bf2f296fb9dab3
traynham <jesse@traynham.com>
2025-07-18 17:04:33 -0500

    Initial commit.

---
+++ static/public/js/plugins/man.js
@@ -0,0 +1,87 @@
+export default {
+	
+	// Basic Information
+	name: 'man',
+	author: 'Jesse Traynham',
+	category: 'Info',
+	description: 'Display a manual.',
+	kind: 'single',
+	syntax: '[man: "plugin"]',
+	version: '1.0.0',
+
+	docs: {
+		flags: {
+			'html': 'Return out put in html',
+			'text': 'Return out put in text',
+			'markdown': 'Return out put in markdown'
+		}
+	},
+
+	// Content and Params details
+	content: [], // No content needed as this works based on the request path
+
+
+	// Examples for usage
+	examples: [
+		{
+			payload: { _request: { path: '/home/about' } },
+			input: '[breadcrumbs]',
+			output: '<div style="--bs-breadcrumb-divider: \'›\';" aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item"><a href="/home">home</a></li><li class="breadcrumb-item">about</li></ol></div>',
+			note: 'This example shows how breadcrumb navigation is generated for the path "/home/about".'
+		}
+	],
+
+
+	
+	processor(req) {
+		
+		return 'THIS is THE man.'
+		
+		console.log('THE MAN!')
+		
+		let theThing = req.engine.plugins[req.content]
+		
+		console.log('MAN::', req.engine.plugins[req.content])
+		
+		return theThing
+		
+		
+		/*
+		let md = (content) => req.engine.runPlugin('md', {content})
+		let md_code = (content) => req.engine.runPlugin('md', {content: '\n```json\n' + content + '\n```\n'})
+		
+		if(req.cargo.flags.includes('keys')){
+			let content =  '## Payload Keys\n' + Object.keys(req.payload).map(key => `* ${key}`).join('\n')
+			return md(content)
+		}
+		
+		if(req.contents.length > 0){
+			let out =  req.contents.map(content => '\n```json\n' + JSON.stringify(content, null, 2) + '\n```\n' )
+			return md(out.join(''))
+		}
+		
+		/*
+			# TODO:
+			* Should session be there?
+			* Session is in both the root and in locals.
+			* Should there be an option for --showAll
+			* Use encode delimiter for replacing delims.
+		* /
+		let exclusions = [
+			'textSynth',
+			'_layoutStack',
+			'settings',
+			'_locals',
+			'_synth'
+		]
+		
+		let pay = Object.keys(req.payload).reduce((acc, key) => {
+			if (!exclusions.includes(key)) { acc[key] = req.payload[key] }
+			return acc;
+		}, {})
+		
+		return md_code( JSON.stringify(pay, null, 2).replaceAll('[', '\\[').replaceAll(']', '\\]') )
+		*/
+	}
+
+}
