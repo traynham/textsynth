054ac34ba50b1b9ebb2b7836ffdf8bcc8ec21c0b
traynham <jesse@traynham.com>
2025-07-18 17:00:00 -0500

    Added support for browser.

--- src/plugins/util/include.js
+++ src/plugins/util/include.js
@@ -23,6 +23,16 @@ export default {
 	],
 	
 	processor(req) {
+		if (req.engine.env.platform === 'node') {
+			return this.onProcessNode(req)
+		} else {
+			return this.onProcessBrowser(req)
+		}
+	},
+	
+	onProcessNode(req) {
+		
+		// TODO: Add ability to get from a url using fetch?
 		
 		// Check if content (the file to be included) is provided. If not, return an error message.
 		if (!req.content) {
@@ -33,7 +43,7 @@ export default {
 		if(!path.extname(req.content)){
 			return 'ERROR: Include file name requires an extension.'
 		}
-		
+// MOVE TO REQ.ENGINE.ENV.VIEWS??????		
 		// Generate the absolute path of the file to be included.
 		let includePath = path.join(req.payload._synth.views, req.content)
 		
@@ -43,7 +53,12 @@ export default {
 		}
 		
 		// If the file exists, read its content.
-		const fileContent = fs.readFileSync(includePath, {encoding:'utf8'})
+		let fileContent = fs.readFileSync(includePath, {encoding:'utf8'})
+		
+		// -removeTabs flag
+		if(req.cargo.flags.includes('removeTabs')){
+			fileContent = fileContent.replaceAll('\t', '')
+		}
 		
 		// Process the included file content using the engine, passing in the file's content and the payload.
 		// This allows nested includes and processing of other tags within the included file.
@@ -52,6 +67,37 @@ export default {
 		// Return the processed content of the included file.
 		return mergedContent
 		
+	},
+
+	onProcessBrowser(req) {
+		
+		if (!req.content) return 'ERROR: No file specified for include tag'
+		
+		//const content = this.fetchSync(req.content)
+		const content = req.engine.fetchSync(req.content)
+		const processed = req.engine.process(content, req.payload)
+		
+		if(req.cargo.flags.includes('markdown')){
+			return req.engine.runPlugin('markdown', { content: processed})
+		} else {
+			return processed
+		}
+		
+	},
+/*
+	fetchSync(path) {
+		
+		const xhr = new XMLHttpRequest()
+		xhr.open('GET', path, false) // false = synchronous
+		xhr.send(null)
+		
+		if (xhr.status === 200) {
+			return xhr.responseText
+		}
+		
+		return `ERROR: Failed to load ${path}: ${xhr.status}`
+		//throw new Error(`Failed to load ${path}: ${xhr.status}`)
+		
 	}
-  
+*/
 }
