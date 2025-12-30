b8c52105c42679396fe9e1793262e14963bedd4c
traynham <jesse@traynham.com>
2025-12-29 18:09:46 -0600

    Added initial docs work.

--- src/plugins/array_and_text/length.js
+++ src/plugins/array_and_text/length.js
@@ -1,41 +1,44 @@
 export default {
 	
-	// Basic Information
 	name: 'length',
+	
 	author: 'Jesse Traynham',
-	description: 'Returns the length of a string or the number of elements in an array.',
-	version: '1.0.0',
 	category: 'Array/Text',
+	description: 'Returns the length of a string or the number of elements in an array.',
 	kind: 'single',
-	syntax: '[length: property.path]',
-
-	// Content and Params details
-	content: [
-		{
-			name: 'content',
-			type: ['string', 'array'],
-			required: true,
-			description: 'Content (string or array) whose length is to be determined.'
-		}
-	],
-
-	// Examples for usage
-	examples: [
-		{
-			payload: "{ text: 'This is 10' }",
-			input: '[length: text]',
-			output: '10',
-			note: 'The content is a string of length 10.'
-		},
-		{
-			payload: "{ array: ['one', 'two', 'three', 'four', 'five'] }",
-			input: '[length: array]',
-			output: '5',
-			note: 'The content is an array of length 5.'
-		},
-	],
-
-	// Processor logic
+	version: '1.0.0',
+	
+	docs: {
+		
+		content: [
+			{
+				name: 'content',
+				type: ['string', 'array'],
+				required: true,
+				description: 'Content (string or array) whose length is to be determined.'
+			}
+		],
+		
+		/*
+		examples: [
+			{
+				payload: "{ text: 'This is 10' }",
+				input: '[length: text]',
+				output: '10',
+				note: 'The content is a string of length 10.'
+			},
+			{
+				payload: "{ array: ['one', 'two', 'three', 'four', 'five'] }",
+				input: '[length: array]',
+				output: '5',
+				note: 'The content is an array of length 5.'
+			},
+		],
+		*/
+		
+		syntax: '[length: property.path]',
+	},
+	
 	processor(req) {
 		if(!req.content){ return 0 }
 		return req.content.length;
